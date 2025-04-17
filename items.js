3361e2f6dfe28f4debdbbd65d8b55d0772bcedab
scalpino <scalpino@wisc.edu>
2025-04-17 15:05:33 -0500

    updated items.js to include shoes

---
+++ items.js
@@ -0,0 +1,22 @@
+function getItems() {
+    return {
+        accessories: [
+            {image: "", name: "", brand: "", price: ""},
+            {image: "", name: "", brand: "", price: ""},
+            {image: "", name: "", brand: "", price: ""},
+            {image: "", name: "", brand: "", price: ""}
+        ],
+        apparel: [
+            {image: "", name: "", brand: "", price: ""},
+            {image: "", name: "", brand: "", price: ""},
+            {image: "", name: "", brand: "", price: ""},
+            {image: "", name: "", brand: "", price: ""}
+        ],
+        shoes: [
+            {image: "images/ua_phantom_1.png", name: "UA Phantom 1", brand: "Underarmor", price: "$120"},
+            {image: "images/AIR+MAX+DN__1_-removebg-preview.png", name: "Nike Air Max Dn", brand: "Nike", price: "$160"},
+            {image: "images/Adizero_EVO_SL_Shoes_White_JH6206_01_00_standard-removebg-preview.png", name: "Adizero EVO SL", brand: "Adidas", price: "$150"},
+            {image: "images/Chuck 70 Canvas.png", name: "Chuck 70 Canvas", brand: "Converse", price: "$85"}
+        ],
+    }
+}
