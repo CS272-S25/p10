96d31d95811796bb8d818d9ca63e93ab631d9a85
scalpino <scalpino@wisc.edu>
2025-04-18 21:04:04 -0500

    fixed pathing for items.js

--- js/items.js
+++ js/items.js
@@ -10,10 +10,10 @@ function getItems() {
             {id: "appa_2", image: "", name: "", brand: "", price: ""},
             {id: "appa_3", image: "", name: "", brand: "", price: ""},
             {id: "appa_4", image: "", name: "", brand: "", price: ""},
-            {id: "shoe_1", image: "images/shoes/ua_phantom_1.png", name: "UA Phantom 1", brand: "Underarmor", price: "$120"},
-            {id: "shoe_2", image: "images/shoes/AIR+MAX+DN__1_-removebg-preview.png", name: "Nike Air Max Dn", brand: "Nike", price: "$160"},
-            {id: "shoe_3", image: "images/shoes/Adizero_EVO_SL_Shoes_White_JH6206_01_00_standard-removebg-preview.png", name: "Adizero EVO SL", brand: "Adidas", price: "$150"},
-            {id: "shoe_4", image: "images/shoes/Chuck 70 Canvas.png", name: "Chuck 70 Canvas", brand: "Converse", price: "$85"}
+            {id: "shoe_1", image: "../images/shoes/ua_phantom_1.png", name: "UA Phantom 1", brand: "Underarmor", price: "$120"},
+            {id: "shoe_2", image: "../images/shoes/AIR+MAX+DN__1_-removebg-preview.png", name: "Nike Air Max Dn", brand: "Nike", price: "$160"},
+            {id: "shoe_3", image: "../images/shoes/Adizero_EVO_SL_Shoes_White_JH6206_01_00_standard-removebg-preview.png", name: "Adizero EVO SL", brand: "Adidas", price: "$150"},
+            {id: "shoe_4", image: "../images/shoes/Chuck 70 Canvas.png", name: "Chuck 70 Canvas", brand: "Converse", price: "$85"}
         ]
     }
 }
