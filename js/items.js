09b66060487c3943e0246360c1c0c4c5ffc7e4c1
Hulises Gonzalez <hgo.23x@gmail.com>
2025-04-18 19:08:04 -0500

    added folders, fixed pathing, and changed bg

---
+++ js/items.js
@@ -0,0 +1,19 @@
+function getItems() {
+    return {
+        items: [
+            //these ids must be consistent across all files
+            {id: "acc_1", image: "", name: "", brand: "", price: ""},
+            {id: "acc_2", image: "", name: "", brand: "", price: ""},
+            {id: "acc_3", image: "", name: "", brand: "", price: ""},
+            {id: "acc_4", image: "", name: "", brand: "", price: ""},
+            {id: "appa_1", image: "", name: "", brand: "", price: ""},
+            {id: "appa_2", image: "", name: "", brand: "", price: ""},
+            {id: "appa_3", image: "", name: "", brand: "", price: ""},
+            {id: "appa_4", image: "", name: "", brand: "", price: ""},
+            {id: "shoe_1", image: "images/ua_phantom_1.png", name: "UA Phantom 1", brand: "Underarmor", price: "$120"},
+            {id: "shoe_2", image: "images/AIR+MAX+DN__1_-removebg-preview.png", name: "Nike Air Max Dn", brand: "Nike", price: "$160"},
+            {id: "shoe_3", image: "images/Adizero_EVO_SL_Shoes_White_JH6206_01_00_standard-removebg-preview.png", name: "Adizero EVO SL", brand: "Adidas", price: "$150"},
+            {id: "shoe_4", image: "images/Chuck 70 Canvas.png", name: "Chuck 70 Canvas", brand: "Converse", price: "$85"}
+        ]
+    }
+}
