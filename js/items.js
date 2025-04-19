af55f0b65fc372b1d090d074071c6104c09c3872
scalpino <scalpino@wisc.edu>
2025-04-18 22:04:53 -0500

    added accessories

--- js/items.js
+++ js/items.js
@@ -2,14 +2,14 @@ function getItems() {
     return {
         items: [
             //these ids must be consistent across all files
-            {id: "acc_1", image: "", name: "", brand: "", price: ""},
-            {id: "acc_2", image: "", name: "", brand: "", price: ""},
-            {id: "acc_3", image: "", name: "", brand: "", price: ""},
-            {id: "acc_4", image: "", name: "", brand: "", price: ""},
+            {id: "acc_1", image: "../images/accessories/stanley-cooler.png", name: "Stanley Mini Backpack Cooler", brand: "Stanley", price: "$125"},
+            {id: "acc_2", image: "../images/accessories/green-gatorade-bottle.png", name: "Gatorade 32 oz. Contour Squeeze Bottle", brand: "Gatorade", price: "$6"},
+            {id: "acc_3", image: "../images/accessories/24NLKUWTRPRFTWSRBTCH-removebg-preview.png", name: "iLIVE Waterproof TWS Earbuds", brand: "iLIVE", price: "$40"},
+            {id: "acc_4", image: "../images/accessories/21NIKUNKLPHFTBLLTFTA_Black_White-removebg-preview.png", name: "Nike Alpha Football Towel", brand: "Nike", price: "$18"},
             {id: "appa_1", image: "../images/apparel/fanatics-red-badger-shirt.png", name: "Wisconsin Badgers T-Shirt", brand: "Fanatics", price: "$24"},
             {id: "appa_2", image: "../images/apparel/fanatics-red-badger-hat.png", name: "Wisconsin Badgers Adjustable Hat", brand: "Fanatics", price: "$25"},
-            {id: "appa_3", image: "../images/apparel/24NIKMWNBLGNTHSTDWNBA.png", name: "WNBA League Crewneck Sweatshirt", brand: "Nike", price: "85"},
-            {id: "appa_4", image: "../images/apparel/lions-hat.png", name: "Detroit Lions Snapback Hat", brand: "New Era", price: "40"},
+            {id: "appa_3", image: "../images/apparel/24NIKMWNBLGNTHSTDWNBA.png", name: "WNBA League Crewneck Sweatshirt", brand: "Nike", price: "$85"},
+            {id: "appa_4", image: "../images/apparel/lions-hat.png", name: "Detroit Lions Snapback Hat", brand: "New Era", price: "$40"},
             {id: "shoe_1", image: "../images/shoes/ua_phantom_1.png", name: "UA Phantom 1", brand: "Underarmor", price: "$120"},
             {id: "shoe_2", image: "../images/shoes/AIR+MAX+DN__1_-removebg-preview.png", name: "Nike Air Max Dn", brand: "Nike", price: "$160"},
             {id: "shoe_3", image: "../images/shoes/Adizero_EVO_SL_Shoes_White_JH6206_01_00_standard-removebg-preview.png", name: "Adizero EVO SL", brand: "Adidas", price: "$150"},
